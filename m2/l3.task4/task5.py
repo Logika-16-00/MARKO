9700c1b353428f4ee5bb8472194f0ae576a0bc0f
Markoharchenko <121375924+Markoharchenko@users.noreply.github.com>
2024-07-22 10:28:46 +0300

    Update task5.py

--- m2/l3.task4/task5.py
+++ m2/l3.task4/task5.py
@@ -9,24 +9,35 @@ bt = QPushButton("Випробувати удачу")
 text = QLabel("Натисніть,щоб взяти участь")
 winner = QLabel("?")
 winner1 = QLabel("?")
+points_label = QLabel("Очки: 0")
+points = 50
 line = QVBoxLayout()
 main_win.resize(400,400)
 line.addWidget(text)
 line.addWidget(winner)
 line.addWidget(winner1)
+line.addWidget(points_label)
 line.addWidget(bt)
-line.addWidget(coins)
 main_win.setLayout(line)
 
 def show_numbers():
+    global points
     number1 = randint(0, 9)
     number2 = randint(0, 9)
     winner.setText(str(number1))
     winner1.setText(str(number2))
     if number1 == number2:
+        
+        points -=10
         text.setText("Ви виграли! Зіграйте знову")
+        points += 40
     else:
+        points -=10
         text.setText("Ви програли! Зіграйте знову")
+        points -= 20
+
+        
+    points_label.setText(f"Очки: {points}")
 
 bt.clicked.connect(show_numbers)
 
