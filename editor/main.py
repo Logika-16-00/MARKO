a5caad4b38e7559452cba66e85e542371be0797a
MarkPG <ytmablo@gmail.com>
2024-11-09 17:23:43 +0200

    final1

--- editor/main.py
+++ editor/main.py
@@ -14,22 +14,26 @@ class Widget(QMainWindow):
         super().__init__()
         self.ui = Ui_MainWindow()
         self.ui.setupUi(self)
+        self.image = None
+
         self.image = Image.open("Губка_БОБ_персонаж.png")
-        self.update_image()
         self.ui.btn_left.clicked.connect(self.rotate_left)
         self.ui.btn_right.clicked.connect(self.rotate_right)
         self.ui.btn_flip.clicked.connect(self.flip_image)
         self.ui.btn_bw.clicked.connect(self.bw_image)
         self.ui.btn_sharp.clicked.connect(self.sharpen_image)
+        self.ui.btn_dir.clicked.connect(self.show_files)
+        self.ui.listWidget.itemClicked.connect(self.show_picture)
         # self.image = Image.open("")
-    def update_image(self):
+    def update_image(self,image = None):
         self.ui.label.hide()
-        self.image.save("Губка_БОБ_персонаж_copy.png")
-        pixmap = QPixmap("Губка_БОБ_персонаж_copy.png")
-        w, h = self.ui.label.width(), self.ui.label.height()
-        pixmap = pixmap.scaled(w, h)
-        self.ui.label.setPixmap(pixmap)
-        self.ui.label.show()
+        if image:
+            self.image.save("copy.png")
+            pixmap = QPixmap("copy.png")
+            w, h = self.ui.label.width(), self.ui.label.height()
+            pixmap = pixmap.scaled(w, h)
+            self.ui.label.setPixmap(pixmap)
+            self.ui.label.show()
 
 
     def rotate_left(self):
@@ -64,9 +68,30 @@ class Widget(QMainWindow):
         res = []
         for file in files:
             for e in ext:
-                if file.endswitch(e):
+                if file.endswith(e):
                     res.append(file)
-        return res        
+        return res    
+
+    def show_files(self):
+        ext = ["png","jpg","PNG","JPG"]
+        self.choose_dir()
+        if workdir:
+            all_files = os.listdir(workdir)
+            print(all_files)
+            filter_files = self.filter(all_files,ext)
+            print(filter_files)
+            self.ui.listWidget.clear()
+            for file in filter_files:
+                self.ui.listWidget.addItem(file)
+
+    def show_picture(self):
+        if self.ui.listWidget.currentRow() >= 0:
+            filename = self.ui.listWidget.currentItem().text()
+            image_path = os.path.join(workdir,filename)
+            self.image = Image.open(image_path)
+            self.update_image(image_path)
+
+
         
 app = QApplication([])
 ex = Widget()
