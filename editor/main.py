0b0d190300a8fb1a679aea05238fa7cbf710835f
MarkPG <ytmablo@gmail.com>
2024-11-09 16:03:41 +0200

    last weak

--- editor/main.py
+++ editor/main.py
@@ -6,9 +6,8 @@ from ui import Ui_MainWindow
 from PIL import Image
 from PIL import ImageFilter
 from PIL import ImageEnhance
-
-
-
+from PyQt5.QtWidgets import QFileDialog
+import os
 
 class Widget(QMainWindow):
     def   __init__(self):
@@ -19,26 +18,60 @@ class Widget(QMainWindow):
         self.update_image()
         self.ui.btn_left.clicked.connect(self.rotate_left)
         self.ui.btn_right.clicked.connect(self.rotate_right)
-
+        self.ui.btn_flip.clicked.connect(self.flip_image)
+        self.ui.btn_bw.clicked.connect(self.bw_image)
+        self.ui.btn_sharp.clicked.connect(self.sharpen_image)
+        # self.image = Image.open("")
     def update_image(self):
         self.ui.label.hide()
         self.image.save("Губка_БОБ_персонаж_copy.png")
-        pixmap = QPixmap("Губка_БОБ_персонаж.png")
+        pixmap = QPixmap("Губка_БОБ_персонаж_copy.png")
         w, h = self.ui.label.width(), self.ui.label.height()
         pixmap = pixmap.scaled(w, h)
         self.ui.label.setPixmap(pixmap)
         self.ui.label.show()
-    
+
+
     def rotate_left(self):
-        self.image.rotate(90)
+        self.image = self.image.rotate(90)
         self.update_image()
+
+    
     def rotate_right(self):
-        self.image.rotate(-90)
+        self.image = self.image.rotate(-90)
         self.update_image()
 
-        
 
+    def flip_image(self):
+        self.image = self.image.transpose(Image.FLIP_LEFT_RIGHT)
+        self.update_image()
+
+    def bw_image(self):
+        self.image = self.image.convert("L")
+        self.update_image()
+
+    def sharpen_image(self):
+        self.image = self.image.filter(ImageFilter.SHARPEN)
+        self.update_image()
+
+
+    def choose_dir(self):
+        global workdir
+        workdir = QFileDialog.getExistingDirectory(self)
+
+        print(workdir)
+    def filter(self,files,ext):
+        res = []
+        for file in files:
+            for e in ext:
+                if file.endswitch(e):
+                    res.append(file)
+        return res        
+        
 app = QApplication([])
 ex = Widget()
 ex.show()
 app.exec_()
+
+
+
