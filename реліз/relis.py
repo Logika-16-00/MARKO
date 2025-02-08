9cc890d1f3168702e3117c4512bece2d807ed396
Markoharchenko <markoharchenko@gmail.com>
2025-02-08 22:32:32 +0200

    relis homework

--- реліз/relis.py
+++ реліз/relis.py
@@ -17,7 +17,8 @@ mixer.music.load("snake.ogg")
 mixer.music.play()
 walls = mixer.Sound("lose.ogg")
 appel = mixer.Sound("ukus.ogg")
-catch = 0
+bubux = mixer.Sound("bomba.ogg")
+catch = 1
 font.init()
 font1 = font.Font(None,30)
 font2 = font.Font(None,80)
@@ -26,6 +27,13 @@ ds_width = 500
 ds_height = 500
 food_x = round(randint(0,400)/10)*10 
 food_y = round(randint(0,400)/10)*10
+
+bomb_x = round(randint(0,400)/10)*10 
+bomb_y = round(randint(0,400)/10)*10
+width, height = 10,10
+snakepos  = [ [x_snake, y_snake]]
+
+lose_sound = False
 while game:
     wn.fill((0,0,0))
     for e in event.get():
@@ -51,34 +59,83 @@ while game:
     if not finish:
         label_catch = font1.render(f'Рахунок: {catch}',True,(255,255,255))
         wn.blit(fon,(0,0))
-        draw.rect(wn,(12,240,2),[x_snake,y_snake,10,10])
+
+        for i in range(0, len(snakepos)):
+            draw.rect(wn, (12,240,2), [snakepos[i], (width, height)])
+            
+        draw.rect(wn,(222,210,2),[bomb_x,bomb_y,10,10])
         draw.rect(wn,(222,10,2),[food_x,food_y,10,10])
         wn.blit(label_catch,(10,10))
-        if x_snake == food_x and y_snake == food_y:
+        if snakepos[0][0] == food_x and snakepos[0][1]  == food_y:
             food_x = round(randint(0,400)/10)*10 
             food_y = round(randint(0,400)/10)*10
             appel.play()
-            catch += 1
-    if x_snake >= ds_width or x_snake <=0:
-        wn.blit(text_lose,(150,225))
-        
-        if x_snake >= ds_width or x_snake <=0:
+            catch +=1
+            
+            snakepos.append(snakepos[-1][:])
+        if snakepos[0][0] == bomb_x and snakepos[0][1]  == bomb_y:
+            bomb_x = round(randint(0,400)/10)*10 
+            bomb_y = round(randint(0,400)/10)*10
+            bubux.play()
+            catch -=1
+
+            del snakepos[0]
+        if snakepos[0][0] >= ds_width or snakepos[0][1] <=0:
+            wn.blit(text_lose,(150,225))
             mixer_music.pause()
             finish =1 
-            walls.play()
+            # walls.play(1)
+                
+                
             
-            
-    if y_snake >= ds_height or y_snake <=0:
-        wn.blit(text_lose,(150,225))
-        
-        if y_snake >= ds_height or y_snake <=0:
+        if snakepos[0][1] >= ds_height or snakepos[0][1] <=0:
+            wn.blit(text_lose,(150,225))
+
             finish =1 
+
+
+            lose_sound = True
+
+
             walls.play()
             mixer_music.pause()
-        
+        if snakepos[0][0] >= ds_width or snakepos[0][0] <=0:
+            wn.blit(text_lose,(150,225))
+
+            finish =1 
+
+
+            lose_sound = True
+
+
+            walls.play()
+            mixer_music.pause()
+      
+        if catch == 0:
+            wn.blit(text_lose,(150,225))
+
+            finish =1 
+
+
+            lose_sound = True
+
+
+            walls.play()
+            mixer_music.pause()
             
-    if start_game:
-            x_snake += x_change
-            y_snake += y_change
+
+            
+        if start_game:
+                for i in range(len(snakepos)-1, 0, -1):
+                    snakepos[i] = snakepos[i-1][:]
+                snakepos[0][0] += x_change
+                snakepos[0][1]  += y_change
+    if snakepos[0][1]  >= ds_height or snakepos[0][1] <=0:
+            wn.blit(text_lose,(125,225))
+    if snakepos[0][0]  >= ds_width or snakepos[0][0] <=0:
+            wn.blit(text_lose,(125,225))
+    if catch < 0:
+            wn.blit(text_lose,(150,225))
+
     display.update()
     clock.tick(fps)
